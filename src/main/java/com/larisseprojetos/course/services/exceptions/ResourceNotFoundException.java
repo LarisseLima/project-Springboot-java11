b0348f98c796957e61381e838f539b3cef347a5a
Larisse Lima <larisse.lima2@gmail.com>
2020-05-27 19:47:31 -0300

    Exception handling - findById

---
+++ src/main/java/com/larisseprojetos/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.larisseprojetos.course.services.exceptions;
+
+public class ResourceNotFoundException  extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+
+}
